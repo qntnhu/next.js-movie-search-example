a56179c573bbda6538b61b4c633efaf572c283c4
qntnhu <quentinhu@fastmail.com>
2017-10-07 10:29:24 +0800

    update test

--- __tests__/pageIndex.test.js
+++ __tests__/pageIndex.test.js
@@ -1,6 +1,6 @@
 /* eslint-env jest */
 import React from 'react';
-import { configure, shallow } from 'enzyme';
+import { configure, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
 import Index from '../pages/index';
 import Result from '../components/Result';
@@ -8,18 +8,21 @@ import Result from '../components/Result';
 configure({ adapter: new Adapter() });
 
 describe('Test index', () => {
-  const index = shallow(<Index />);
+  const index = mount(<Index />);
 
   it('should render a <Result />', () => {
     expect(index.find(Result).length).toBe(1);
   });
 
-  // it('test input keyup event', () => {
-  //   expect(index.find('form > input').get(0).value).toEqual(undefined);
-  //   // index.find('form > input').simulate('keyUp', {
-  //   //   target: { value: 'hello' }
-  //   // });
-  //   index.find('form > input').instance().placeholder = 'hello';
-  //   expect(index.find('form > input').get(0).value).toEqual('hello');
-  // });
+  it('test input keyup event', () => {
+    expect(index.find('form > input').get(0).value).toEqual(undefined);
+    expect(index.state()).toEqual({ key: '', result: null });
+    index.find('form > input').instance().value = 'hello';
+    index.find('form > input').simulate('keyUp');
+    // index.find('form > input').simulate('keyUp', {
+    // target: { value: 'hello' }
+    // });
+    expect(index.find('form > input').instance().value).toEqual('hello');
+    expect(index.state()).toEqual({ key: 'hello', result: null });
+  });
 });
